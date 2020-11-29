6ff2a30d5415a4dedcc8ff3987cfcd184199ee6f
gabrielnunes25 <62342222+gabrielnunes25@users.noreply.github.com>
2020-11-29 11:35:10 -0300

    Criação do Adapter

---
+++ Adapter/AdapterJSON.cs
@@ -0,0 +1,29 @@
+public class AdapterJSON : IAdapterJSON
+{
+    public List<Quarto> LerJSONQuatos()
+    {
+
+    }
+    public void EscreverJSONQuartos()
+    {
+
+    }
+
+    public List<Cliente> LerJSONClientes()
+    {
+
+    }
+    public void EscreverJSONCLiestes()
+    {
+
+    }
+
+    public List<Reserva> LerJSONReservas()
+    {
+
+    }
+    public void EscreverJSONReservas()
+    {
+
+    }
+}
---
+++ Adapter/IAdapterJSON.cs
@@ -0,0 +1,12 @@
+
+public interface IAdapterJSON
+{
+   public List<Quarto> LerJSONQuatos(); 
+   public void EscreverJSONQuartos();
+
+   public List<Cliente> LerJSONClientes(); 
+   public void EscreverJSONCLiestes();
+
+   public List<Reserva> LerJSONReservas(); 
+   public void EscreverJSONReservas();
+}
